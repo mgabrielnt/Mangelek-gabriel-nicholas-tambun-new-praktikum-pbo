c3aad965c3510b0d204566420bcd4875f3a5202e
mgabrielnt <118196810+mgabrielnt@users.noreply.github.com>
2024-03-04 11:38:04 +0700

    Add files via upload

---
+++ praktikum3/AngkaSialException.java
@@ -0,0 +1,10 @@
+/* File : Asersi1.java */
+/* Deskripsi : Exception usaha sendiri nih, menolak angka 13 */
+/* Nama/Nim : Manglek Gabriel Nicholas Tambun /24060122140176 */
+
+public class AngkaSialException extends Exception{
+    public AngkaSialException(){
+        super("jangan memasukkan angka 13 karena sial broo!!");
+
+    }
+}
---
+++ praktikum3/ExceptionOnArray.java
@@ -0,0 +1,21 @@
+/* File : ExceptionOnArray.java */
+/* Deskripsi : Program penggunaan eksepsi menggunakan class library Java */
+/* Nama/Nim : Manglek Gabriel Nicholas Tambun /24060122140176 */
+
+public class ExceptionOnArray{
+    public static void main(String[] args){
+        //instansiasi object array integer
+        
+        Integer[] arrayInteger = new Integer[4];
+        try{
+            arrayInteger[2] = 11;
+            arrayInteger[4] = 10;
+        }
+        catch (ArrayIndexOutOfBoundsException exception){
+            exception.printStackTrace();
+        }
+        finally{
+            System.out.println("clean up code....");
+        }
+    }
+}
